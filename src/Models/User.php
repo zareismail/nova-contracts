ce86a34e3e3b809d9a6ed2c0cebfa1cedca91169
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-12-14 21:36:28 +0330

    Handles Developer scopes

--- src/Models/User.php
+++ src/Models/User.php
@@ -28,11 +28,33 @@ class User extends Authenticatable
         return User::class;
     }
 
+    /**
+     * Determin if the user is Developer.
+     * 
+     * @return boolean
+     */
     public function isDeveloper()
     {
         return $this->email === 'zarehesmaiel@gmail.com';
     }
 
+    /**
+     * Query the deveoper.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query 
+     * @return \Illuminate\Database\Eloquent\Builder        
+     */
+    public function scopeDeveloper($query)
+    {
+        return $query->whereEmail('zarehesmaiel@gmail.com');
+    }
+
+    /**
+     * Query without deveoper.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder $query 
+     * @return \Illuminate\Database\Eloquent\Builder        
+     */
     public function scopeWithoutDeveloper($query)
     {
         return $query->where($query->qualifyColumn('email'), '!=', 'zarehesmaiel@gmail.com');
