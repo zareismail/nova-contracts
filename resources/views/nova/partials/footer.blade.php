04771c653933ef93ef26797f5840decfbbef68c7
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-12-23 23:08:52 +0330

    removes Laravel LLC

---
+++ resources/views/nova/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<p class="mt-8 text-center text-xs text-80">
+    <a href="http://zareismail.cvbuilder.me/en" target="_blank" class="text-primary dim no-underline">Terme</a>
+    <span class="px-1">&middot;</span>
+    &copy; {{ date('Y') }} Ismail Zare.
+    <span class="px-1">&middot;</span>
+    v{{ \Laravel\Nova\Nova::version() }}
+</p>
